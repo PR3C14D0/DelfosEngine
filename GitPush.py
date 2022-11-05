079ca564a80922596a41bb2f9a9c11b2025f6442
PR3C14D0 <aarongarpre@gmail.com>
2022-11-05 21:54:26 +0100

    Created GitPush.py script

---
+++ GitPush.py
@@ -0,0 +1,23 @@
+import argparse, os
+
+branches = ['master', '0.1.0']
+
+def main():
+    parser = argparse.ArgumentParser(prog = "GitPush", description = "Pushes git changes", epilog = "--message <Commit message>")
+    parser.add_argument("-m", "--message")
+    args = parser.parse_args()
+
+    for branch in branches:
+        os.system(f"git checkout {branch}")
+        if branch == "master":
+            os.system("git add . --force")
+        else:
+            os.system("git cherry-pick master")
+        
+        os.system(f"git commit -m {args.message}")
+        os.system(f"git push origin {branch}")
+        if branch != "master":
+            os.system("git checkout master")
+
+if __name__ == "__main__":
+    main()
